bb4e09714761030dfd134dde23d31efc7e713706
senthilaravinth <senthilaravinth.s2023@vitstudent.ac.in>
2026-04-17 14:18:51 +0530

    this is a fr

--- src/test/java/com/learn/Primenumber/AppTest.java
+++ src/test/java/com/learn/Primenumber/AppTest.java
@@ -7,20 +7,16 @@ import org.junit.jupiter.api.Test;
 public class AppTest {
 
     @Test
-    public void testIsPrimeWithValidPrimes() {
-        // These should return true
-        assertTrue(App.isPrime(2), "2 should be prime");
+    public void testPrimes() {
+        // Syntax: assertTrue(condition, "message")
         assertTrue(App.isPrime(7), "7 should be prime");
         assertTrue(App.isPrime(13), "13 should be prime");
-        assertTrue(App.isPrime(29), "29 should be prime");
     }
 
     @Test
-    public void testIsPrimeWithNonPrimes() {
-        // These should return false
-        assertFalse(App.isPrime(1), "1 is not prime");
+    public void testNonPrimes() {
         assertFalse(App.isPrime(4), "4 is not prime");
         assertFalse(App.isPrime(10), "10 is not prime");
-        assertFalse(App.isPrime(-5), "Negative numbers are not prime");
+        assertFalse(App.isPrime(1), "1 is not prime");
     }
 }
